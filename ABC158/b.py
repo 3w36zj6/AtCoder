0f28d1b41e71e70d05553a69f817171a3f684f91
ribbon-room <52315048+ribbon-room@users.noreply.github.com>
2020-04-13 13:34:49 +0900

    no message

--- ABC158/b.py
+++ ABC158/b.py
@@ -1,14 +1,5 @@
 N, A, B = map(int,input().split(" "))
 
-cnt_a = cnt_b = 0
-
-while True:
-    if cnt_a + cnt_b + A >= N: 
-        # N - cnt_a - cnt_b はみ出た分
-        ans = cnt_a + N - cnt_a - cnt_b
-        break
-    else:
-        cnt_a += A
-        cnt_b += B
+ans = (N//(A+B))*A + min((N%(A+B)),A)
 
 print(ans)
--- ABC158/d.py
+++ ABC158/d.py
@@ -4,14 +4,24 @@ from collections import deque
 S = deque(input())
 Q = int(input())
 QUERY = [input().split(" ") for i in range(Q)]
+reverse_f = 0
 
 for query in QUERY:
-    #print(query)
     if query[0] == "1":#反転
-        S.reverse()
+        reverse_f = (reverse_f+1)%2
     else:#=="2"
         if query[1] == "1":
-            S.appendleft(query[2])
+            if reverse_f:
+                S.append(query[2])
+            else:
+                S.appendleft(query[2])
+
         else:#=="2"
-            S.append(query[2])
+            if reverse_f:
+                S.appendleft(query[2])
+            else:
+                S.append(query[2])
+
+if reverse_f:
+    S.reverse()
 print("".join(list(S)))
